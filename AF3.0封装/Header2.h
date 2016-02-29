32697c7255f0884827e6d7fac380c1977cc2dc10
yingQiangShen <1528787321@qq.com>
2016-02-29 17:43:27 +0800

    提交

--- AF3.0封装/Header2.h
+++ AF3.0封装/Header2.h
@@ -1,15 +1,13 @@
 //
-//  Header.h
+//  Header2.h
 //  AF3.0封装
 //
 //  Created by syq on 16/2/29.
 //  Copyright © 2016年 lanou.syq. All rights reserved.
 //
 
-#ifndef Header_h
-#define Header_h
+#ifndef Header2_h
+#define Header2_h
 
-////**
-// *
-// */
-#endif /* Header_h */
+
+#endif /* Header2_h */
